af5516776b57feeda52b7fec5ecc3f8ed74dfeeb
Sanju Kamat <SanjuKamat@gmail.com>
2023-10-12 12:46:05 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,387 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.17551071069681967,
+        "pitch": 0.0993497807486321,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1794484619656442,
+          "pitch": 0.07411143753395422,
+          "rotation": 0,
+          "target": "1-restaurant--bar"
+        },
+        {
+          "yaw": -2.1120644641251154,
+          "pitch": 0.16190051968407815,
+          "rotation": 0,
+          "target": "2-restaurant--bar"
+        },
+        {
+          "yaw": 0.6788880419854628,
+          "pitch": 0.08597604782047696,
+          "rotation": 0,
+          "target": "3-restaurant--bar"
+        },
+        {
+          "yaw": -3.054311899680876,
+          "pitch": 0.09807860107532385,
+          "rotation": 0,
+          "target": "5-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.2383192466350934,
+        "pitch": -0.04301202861546116,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.457179605128223,
+          "pitch": 0.03909150868866362,
+          "rotation": 0,
+          "target": "2-restaurant--bar"
+        },
+        {
+          "yaw": 1.9586579484052313,
+          "pitch": 0.06018801767763193,
+          "rotation": 0,
+          "target": "6-restaurant--bar"
+        },
+        {
+          "yaw": 0.278314507697079,
+          "pitch": 0.13782127064488137,
+          "rotation": 0,
+          "target": "4-restaurant--bar"
+        },
+        {
+          "yaw": -0.2597900046566668,
+          "pitch": 0.05515341426784737,
+          "rotation": 0,
+          "target": "3-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.2187164777014594,
+        "pitch": 0.07077248038470785,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4135488710345605,
+          "pitch": 0.10820802077232727,
+          "rotation": 0,
+          "target": "1-restaurant--bar"
+        },
+        {
+          "yaw": 0.20127461124984336,
+          "pitch": 0.10669230551797426,
+          "rotation": 0,
+          "target": "3-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.728193509081148,
+        "pitch": 0.15361101705350322,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1397166974158974,
+          "pitch": 0.08707897797224362,
+          "rotation": 0,
+          "target": "0-restaurant--bar"
+        },
+        {
+          "yaw": 2.0008557813882435,
+          "pitch": 0.12137322298777597,
+          "rotation": 0,
+          "target": "4-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.42462412533990346,
+          "pitch": 0.04872333465856826,
+          "rotation": 0,
+          "target": "0-restaurant--bar"
+        },
+        {
+          "yaw": 0.7554577290503772,
+          "pitch": 0.04898045010619256,
+          "rotation": 0,
+          "target": "3-restaurant--bar"
+        },
+        {
+          "yaw": -1.9887465294391653,
+          "pitch": 0.047531988974593986,
+          "rotation": 4.71238898038469,
+          "target": "6-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.2460927145695386,
+        "pitch": 0.4164753326356845,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4001648614763464,
+          "pitch": 0.053067467132210666,
+          "rotation": 0,
+          "target": "0-restaurant--bar"
+        },
+        {
+          "yaw": -2.6638573730349364,
+          "pitch": 0.05647850760290041,
+          "rotation": 0,
+          "target": "4-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.978479049061482,
+        "pitch": 0.1691601725225631,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6447377501313705,
+          "pitch": 0.12218310794472131,
+          "rotation": 0,
+          "target": "7-restaurant--bar"
+        },
+        {
+          "yaw": -1.0707073896240011,
+          "pitch": 0.059352894072661755,
+          "rotation": 0,
+          "target": "0-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-restaurant--bar",
+      "name": "Restaurant & Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.730035604106363,
+        "pitch": 0.17632981031771244,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0554049519111501,
+          "pitch": 0.15943782353432923,
+          "rotation": 0,
+          "target": "2-restaurant--bar"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Restaurant",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
